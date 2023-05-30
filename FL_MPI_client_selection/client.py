fbb930da7f256f75accd9721b737a8fa3fff398f
Shilong Wang <133764656+slwang-ustc@users.noreply.github.com>
2023-05-30 23:57:35 +0800

    Add files via upload

---
+++ FL_MPI_client_selection/client.py
@@ -0,0 +1,24 @@
+class ClientConfig:
+    def __init__(self, idx):
+        self.idx = idx
+        # self.params = None
+        self.params_dict = None
+        self.epoch_idx = 0
+        # self.params = None
+
+        self.train_data_idxes = None
+        # self.model_type = None
+        # self.dataset_type = None
+        # self.batch_size = None
+        self.lr = None
+        # self.decay_rate = None
+        # self.min_lr = None
+        # self.epoch = None
+        # self.momentum = None
+        # self.weight_decay = None
+        # self.local_steps = 20
+
+        self.aggregate_weight = 0.1
+
+        self.train_time = 0
+        self.send_time = 0
