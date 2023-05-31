8909baadeb8ed69075413734481cc22efe42298e
Shilong Wang <133764656+slwang-ustc@users.noreply.github.com>
2023-05-31 14:31:02 +0800

    Update cnn.py

--- models/cnn.py
+++ models/cnn.py
@@ -19,29 +19,23 @@ class CNN(nn.Module):
         torch.manual_seed(cfg['model_init_seed'])
 
         # head
-        blocks = [
-            nn.Conv2d(data_shape[0], hidden_size[0], 3, 1, 1),
-        ]
+        blocks = [nn.Conv2d(data_shape[0], hidden_size[0], 3, 1, 1)]
 
         for i in range(len(hidden_size) - 1):
-            blocks.extend(
-                [
-                    nn.Conv2d(hidden_size[i], hidden_size[i + 1], 3, 1, 1),
-                    nn.ReLU(inplace=True),
-                    nn.MaxPool2d(kernel_size=2, stride=2)
-                ]
-            )
+            blocks.extend([
+                nn.Conv2d(hidden_size[i], hidden_size[i + 1], 3, 1, 1),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=2, stride=2)
+            ])
 
         # classifier
-        blocks.extend(
-            [
-                nn.Flatten(),
-                nn.Linear(
-                    int(hidden_size[-1] * cfg['data_shape'][1] * cfg['data_shape'][2] / (4 ** (len(hidden_size)-1))), 
-                    classes_size
-                )
-            ]
-        )
+        blocks.extend([
+            nn.Flatten(),
+            nn.Linear(
+                int(hidden_size[-1] * cfg['data_shape'][1] * cfg['data_shape'][2] / (4 ** (len(hidden_size)-1))), 
+                classes_size
+            )
+        ])
 
         self.blocks = nn.Sequential(*blocks)
 
